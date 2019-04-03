ec50dcbb4420bbf7cbe15c2d9ef6b20765fc0748
Chris Black <black@chrisblack.net>
2019-04-03 13:10:23 -0700

    Just Blake
    Just going with Blake's "ABC" monologue.

--- assets/screenplay.js
+++ assets/screenplay.js
@@ -1,37 +1,45 @@
 /**
- * Script text from:
- * https://www.springfieldspringfield.co.uk/movie_script.php?movie=glengarry-glen-ross
- * I selected two monologues, Blake's "ABC" speeach, and Roma's philosophizing sales pitch.
- * Edited for line breaks, and also took out a few words which would have made this funny
- * little bot into a hurtful nightmare. I replaced those words with "(expletive)"
- * which is somehow funnier.
+ * I selected Blake's "ABC" scene.
+ * Edited for line breaks, and also took out a word which didn't sit well in this context. 
+ * I replaced that with "(expletive)"  which is somehow funnier.
  */
 
 const dialogue = `
 Let me have your attention for a moment.
-You're talking about what? You're talking about ... 
-bitching about that sale you shot ... some son of a bitch don't want to buy land ...
-somebody don't want what you're selling ... some broad you're trying to screw, so forth.
-Let's talk about something important. Are they all here? I'm going anyway.
+You're talking about what? 
+You're talking about ... bitching about that sale you shot ...
+some son of a bitch don't want to buy land ... 
+somebody don't want what you're selling ...
+some broad you're trying to screw, so forth.
+Let's talk about something important.
+Are they all here? 
+All but one.
+I'm going anyway.
 Let's talk about something important.
 Put that coffee down.
 Coffee's for closers only.
 You think I'm fucking with you?
 I am not fucking with you.
-I'm here from downtown. I'm here from Mitch and Murray.
+I'm here from downtown.
+I'm here from Mitch and Murray.
 And I'm here on a mission of mercy.
-Your name's Levene? You call yourself a salesman, you son of a bitch?
+Your name's Levene? 
+Yeah.
+You call yourself a salesman, you son of a bitch?
 I don't got to listen to this shit.
 You certainly don't, 'cause the good news is, you're fired.
-The bad news is you've got ... all of you've got ... just one week to regain your jobs, 
-starting with tonight ... ... starting with tonight's sit.
+The bad news is you've got ... all of you've got ... 
+just one week to regain your jobs, starting with tonight ... 
+... starting with tonight's sit.
 Oh, have I got your attention now?
 Good ... 'cause we're adding a little something to this month's sales contest.
 As you all know, first prize is a Cadillac Eldorado.
-Anybody want to see second prize? Second prize is a set of steak knives.
+Anybody want to see second prize?
+Second prize is a set of steak knives.
 Third prize is you're fired.
 Do you get the picture? Are you laughing now?
-You got leads. Mitch and Murray paid good money. Get their names to sell them.
+You got leads. Mitch and Murray paid good money. 
+Get their names to sell them.
 You can't close the leads you're given, you can't close shit, you are shit!
 Hit the bricks, pal, and beat it, 'cause you are going out!
 The leads are weak. 
@@ -63,7 +71,9 @@ Crap.
 What's the problem, pal? You ... Moss.
 You're such a hero, you're so rich, 
 ... how come you're here wasting your time with a bunch of bums?
-You see this watch? You see this watch? That watch costs more than your car.
+You see this watch? You see this watch? 
+Yeah.
+That watch costs more than your car.
 I made $970,000 last year. How much you make?
 You see, pal ... that's who I am, and you're nothing.
 Nice guy? I don't give a shit.
@@ -76,72 +86,23 @@ I can go out there tonight ... the materials you got ... make myself $15,000.
 Tonight ... in two hours! Can you? Can you?
 Go and do likewise. A-I-D-A.
 Get mad, you son of a bitches. Get mad!
-You know what it takes to sell real estate? It takes brass balls to sell real estate.
+You know what it takes to sell real estate? 
+It takes brass balls to sell real estate.
 Go and do likewise, gents.
-The money's out there ... you pick it up, it's yours, you don't, I got no sympathy for you.
+The money's out there ... 
+... you pick it up, it's yours, you don't, I got no sympathy for you.
 You want to go out on those sits tonight and close ... close ... it's yours.
 Not, you're gonna be shining my shoes.
 And you know what you'll be saying. Bunch of losers sitting around in a bar, 
 Oh yeah, I used to be a salesman. It's a tough racket.
-These are the new leads. These are the Glengarry leads. And to you, they're gold. 
-And you don't get them. Why? Because to give them to you is just throwing them away. 
+These are the new leads. 
+These are the Glengarry leads. And to you, they're gold. And you don't get them. 
+Why? Because to give them to you is just throwing them away. 
 They're for closers.
 I'd wish you good luck, but you wouldn't know what to do with it if you got it.
 And to answer your question, pal ... Why am I here? 
-I came here because Mitch and Murray asked me to.
-They asked me for a favor. 
-I said, The real favor ... follow my advice and fire your fucking ass, because a loser is a loser.
-All train compartments smell vaguely of shit.
-It gets so you don't mind it.
-That's the worst thing that I can confess.
-You know how long it took me to get there? A long time.
-When you die you're gonna regret the things you don't do.
-You think you're queer? I'm gonna tell you something. We're all queer. 
-You think you're a thief? So what?
-You get befuddled by a middle-class morality? Get shut of it. Shut it out.
-You cheat on your wife, you did it. Live with it. You fuck little girls, so be it.
-There's an absolute morality? Maybe. And then what?
-If you think there is, go ahead, be that thing. Bad people go to hell? I don't think so.
-You think that, act that way. 
-A hell exists on earth? Yes. I won't live in it. That's me.
-Did you ever take a dump, made you feel like you slept for 12 hours? Or a piss?
-Great meals fade in reflection. Everything else gains. Do you know why?
-'Cause it's only food. This shit we put in us, keeps us going ... it's only food.
-The great fucks you may have had, what do you remember about them?
-I don't know. For me, I'm saying what it is, it's probably not the orgasm.
-Some broad's forearm on your neck, something her eyes did. There was this sound she made.
-Or it's me in the ... I'm telling you ...
-I'm in bed the next day, she brought me cafe au lait,
-gives me a cigarette, my balls feel like concrete.
-Eh? What I'm saying, what is our life?
-Our life is looking forward or it's looking back.
-That's it. That's our life.
-Where's the moment?
-And what is it we're so afraid of? Loss. What else?
-The bank closes, we get sick, my wife died on a plane?
-The stock market collapsed? What of these things happen? None of them. 
-We worry anyway. Why?
-You know, they say, You don't buy it, you rent it. The thing.
-You really ... what do you keep? I mean, you don't keep anything, really.
-Security. Things. Things. You know? 
-It's just ... you try to stave off insecurity. You can't do it.
-No. That's what I'm telling you.
-Stocks, bonds, objects of art, real estate, what are they?
-An opportunity. To what?
-To make money? Perhaps. To lose money? Perhaps.
-To indulge and to learn about ourselves? Perhaps.
-So fucking what? What is it? They're an opportunity.
-That's all they are. They're an event.
-A guy comes to you, you make a call.
-You send in a card, I have these properties I would like for you to see. 
-What does this mean? What do you want it to mean?
-You see what I'm saying? Things happen to you.
-Glad I met you. I'm glad I met you, James.
-I want to show you something. It may mean something to you, it may not.
-I don't know. I don't know anymore.
-What is that? Florida. Glengarry Highlands. Florida.
-Bullshit. And maybe that's true. And that's what I said.
-But look at this. What is this? This is a piece of land.
-Listen to what I'm going to tell you now.
+I came here because Mitch and Murray asked me to. They asked me for a favor. 
+I said, The real favor ... follow my advice and fire your fucking ass ... 
+... because a loser is a loser.
 `;
 exports.dialogue = dialogue;
